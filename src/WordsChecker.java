1da3e8608e35c6f4ce254d61693e80a67b8424ad
kmzplanner <kmzplanner@yandex.ru>
2023-04-25 11:41:42 +0300

    Correction
    git commit -a -m Correction
    git commit -a -m Correction

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -4,17 +4,18 @@ import java.util.Set;
 public class WordsChecker {
 
     protected String text;
+    protected Set<String> set = new HashSet<>();
 
     public WordsChecker(String text) {
         this.text = text;
-    }
-
-    public boolean hasWord(String fineWord) {
         String[] words = text.split("\\P{IsAlphabetic}+");
-        Set<String> set = new HashSet<>();
         for (String word : words) {
             set.add(word);
         }
+    }
+
+    public boolean hasWord(String fineWord) {
+
         return set.contains(fineWord);
     }
 }
